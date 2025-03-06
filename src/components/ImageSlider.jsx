6fd5fd25fd6f054fbaca3f9502f3e205cbabb9ac
Richi-Kodehode <richard25sfj@gmail.com>
2025-03-06 12:55:06 +0100

    changes to size

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -13,21 +13,29 @@ export default function ImageSlider() {
   }
 
   return (
-    <div className="max-w-[1400px] m-auto py-16 px-4 relative group rounded-2xl">
-      <div className="flex overflow-hidden rounded-2xl">
+    <div className="max-w-[1024px] m-auto py-3 px-4 relative group rounded-2xl flex justify-center">
+      <div className="aspect-5/4 flex overflow-hidden rounded-2xl">
         <div
           className="flex transition-transform duration-500"
-          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          style={{
+            transform: `translateX(-${currentIndex * 100}%)`,
+            width: `${carImages.length * 100}%`,
+          }}
         >
           {carImages.map((image, index) => (
-            <img key={index} className="h-full w-full" src={image.src} alt="" />
+            <img
+              key={index}
+              className="w-full h-full object-cover"
+              src={image.src}
+              alt=""
+            />
           ))}
         </div>
       </div>
 
       <div
         onClick={previousIndex}
-        className="hidden group-hover:block absolute top-1/2 -translate-y-1/2 left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+        className="hidden group-hover:block absolute top-1/2 -translate-y-1/2 left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer "
       >
         <BsChevronCompactLeft size={30} />
       </div>
